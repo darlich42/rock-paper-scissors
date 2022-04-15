37c347ce6789a33272fa3d7ede9ab0a4f183db38
Dylan Arlich <d.arlich42@gmail.com>
2022-04-14 23:08:28 -0700

    add function computerPlay() for computer to choose between rock paper or scissor

---
+++ javascript.js
@@ -0,0 +1,20 @@
+
+
+//write a function that randomly chooses between rock, paper and scissors
+
+const myArray = ["Rock", "Paper", "Scissors"];
+
+function computerPlay() {
+  return myArray[Math.floor(Math.random() * myArray.length)];
+}
+
+
+
+//write a function where the user can input rock paper or scissors. will be not case sensitive. will return a message if it
+//not one of the 3 options
+
+//game function will compare the two inputs and determine the winner
+// use "or" statements to shorten the list or winner/loser messages
+
+
+//create a function to track 5 rounds and determine the ultimate winner
