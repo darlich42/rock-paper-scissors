502bbfbad926cfca66404de3383e8782c006fd30
Dylan Arlich <d.arlich42@gmail.com>
2022-05-15 15:55:00 -0700

    Finish coding game assignment

--- javascript.js
+++ javascript.js
@@ -1,20 +1,80 @@
+const choices = ["rock", "paper", "scissors"];
+const winners = [];
 
-
-//write a function that randomly chooses between rock, paper and scissors
-
-const myArray = ["Rock", "Paper", "Scissors"];
-
-function computerPlay() {
-  return myArray[Math.floor(Math.random() * myArray.length)];
+function game() {
+  for (let i = 1; i <= 5; i++) {
+    playRound(i);
+  }
+  document.querySelector("button").textContent = "Play Again";
+  logWins();
 }
 
+function playRound(round) {
+  const playerSelection = playerChoice();
+  const computerSelection = computerChoice();
+  const winner = checkWinner(playerSelection, computerSelection);
+  winners.push(winner);
+  logRound(playerSelection, computerSelection, winner, round);
+}
 
+function playerChoice() {
+  let input = prompt("Type Rock, Paper, or Scissors!", "paper");
+  while (input == null) {
+    input = prompt("Type Rock, Paper, or Scissors!", "paper");
+  }
+  input = input.toLowerCase();
+  let check = validateInput(input);
+  while (check == false) {
+    input = prompt(
+      "Type Rock, Paper, or Scissors! With Correct spelling please.",
+      "paper"
+    );
+    input = input.toLowerCase();
+    check = validateInput(input);
+  }
+  return input;
+}
 
-//write a function where the user can input rock paper or scissors. will be not case sensitive. will return a message if it
-//not one of the 3 options
+function computerChoice() {
+  return choices[Math.floor(Math.random() * choices.length)];
+}
 
-//game function will compare the two inputs and determine the winner
-// use "or" statements to shorten the list or winner/loser messages
+function validateInput(choice) {
+  if (choices.includes(choice)) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
+function checkWinner(choiceP, choiceC) {
+  if (choiceP === choiceC) {
+    return "tie";
+  } else if (
+    (choiceP === "rock" && choiceC === "scissors") ||
+    (choiceP === "paper" && choiceC === "rock") ||
+    (choiceP === "scissors" && choiceC === "paper")
+  ) {
+    return "player";
+  } else {
+    return "computer";
+  }
+}
 
-//create a function to track 5 rounds and determine the ultimate winner
+function logWins() {
+  let playerWins = winners.filter((item) => item == "player").length;
+  let computerWins = winners.filter((item) => item == "computer").length;
+  let tieGames = winners.filter((item) => item == "tie").length;
+  console.log("Results: ");
+  console.log("Player Wins: ", playerWins);
+  console.log("Computer Wins: ", computerWins);
+  console.log("Ties: ", tieGames);
+}
+
+function logRound(playerChoice, computerChoice, winner, round) {
+  console.log("Round: ", round);
+  console.log("Player Chose: ", playerChoice);
+  console.log("Computer Chose: ", computerChoice);
+  console.log(winner, "won this round.");
+  console.log("-------------------------------------");
+}
