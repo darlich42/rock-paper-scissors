d4f6da5270679285f496920517276665901d37d8
Dylan Arlich <d.arlich42@gmail.com>
2022-07-12 16:32:08 -0700

    Use Dom methods to track winners and score

--- javascript.js
+++ javascript.js
@@ -1,80 +1,97 @@
 const choices = ["rock", "paper", "scissors"];
 const winners = [];
 
-function game() {
-  for (let i = 1; i <= 5; i++) {
-    playRound(i);
-  }
-  document.querySelector("button").textContent = "Play Again";
-  logWins();
-}
+// function game() {
+//   for (let i = 1; i <= 5; i++) {
+//     playRound(i);
+//   }
+//   document.querySelector("button").textContent = "Play Again";
+//   logWins();
+// }
 
-function playRound(round) {
-  const playerSelection = playerChoice();
+function playRound(e) {
+  const playerSelection = e;
   const computerSelection = computerChoice();
   const winner = checkWinner(playerSelection, computerSelection);
   winners.push(winner);
-  logRound(playerSelection, computerSelection, winner, round);
-}
-
-function playerChoice() {
-  let input = prompt("Type Rock, Paper, or Scissors!", "paper");
-  while (input == null) {
-    input = prompt("Type Rock, Paper, or Scissors!", "paper");
-  }
-  input = input.toLowerCase();
-  let check = validateInput(input);
-  while (check == false) {
-    input = prompt(
-      "Type Rock, Paper, or Scissors! With Correct spelling please.",
-      "paper"
-    );
-    input = input.toLowerCase();
-    check = validateInput(input);
-  }
-  return input;
+  logRound(playerSelection, computerSelection, winner);
+  logWins();
 }
 
 function computerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function validateInput(choice) {
-  if (choices.includes(choice)) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
 function checkWinner(choiceP, choiceC) {
   if (choiceP === choiceC) {
-    return "tie";
+    return "Tie";
   } else if (
     (choiceP === "rock" && choiceC === "scissors") ||
     (choiceP === "paper" && choiceC === "rock") ||
     (choiceP === "scissors" && choiceC === "paper")
   ) {
-    return "player";
+    return "Player";
   } else {
-    return "computer";
+    return "Computer";
   }
 }
 
 function logWins() {
-  let playerWins = winners.filter((item) => item == "player").length;
-  let computerWins = winners.filter((item) => item == "computer").length;
-  let tieGames = winners.filter((item) => item == "tie").length;
-  console.log("Results: ");
-  console.log("Player Wins: ", playerWins);
-  console.log("Computer Wins: ", computerWins);
-  console.log("Ties: ", tieGames);
+  let playerWins = winners.filter((item) => item == "Player").length;
+  let computerWins = winners.filter((item) => item == "Computer").length;
+  let tieGames = winners.filter((item) => item == "Tie").length;
+  scorediv.textContent =
+    "Wins: " +
+    "Player: " +
+    playerWins +
+    " Computer: " +
+    computerWins +
+    " Tie Games: " +
+    tieGames;
+  results.appendChild(scorediv);
+  if (playerWins === 5) {
+    scorediv.textContent = "PLAYER WINS!!";
+    winners.length = 0;
+  } else if (computerWins === 5) {
+    scorediv.textContent = "COMPUTER WINS!! :(";
+    winners.length = 0;
+  }
+  // console.log("Results: ");
+  // console.log("Player Wins: ", playerWins);
+  // console.log("Computer Wins: ", computerWins);
+  // console.log("Ties: ", tieGames);
 }
 
-function logRound(playerChoice, computerChoice, winner, round) {
-  console.log("Round: ", round);
-  console.log("Player Chose: ", playerChoice);
-  console.log("Computer Chose: ", computerChoice);
-  console.log(winner, "won this round.");
-  console.log("-------------------------------------");
+const results = document.querySelector(".results");
+const playerdiv = document.createElement("div");
+playerdiv.classList.add("playerdiv");
+const compdiv = document.createElement("div");
+compdiv.classList.add("compdiv");
+const winnerdiv = document.createElement("div");
+winnerdiv.classList.add("winnerdiv");
+const scorediv = document.createElement("div");
+scorediv.classList.add("scorediv");
+
+function logRound(playerChoice, computerChoice, winner) {
+  playerdiv.textContent = "Player Chose: " + playerChoice;
+  compdiv.textContent = "Computer Chose: " + computerChoice;
+  winnerdiv.textContent = winner + " won this round!";
+  results.appendChild(playerdiv);
+  results.appendChild(compdiv);
+  results.appendChild(winnerdiv);
+
+  // console.log("Player Chose: ", playerChoice);
+  // console.log("Computer Chose: ", computerChoice);
+  // console.log(winner, "won this round.");
+  // console.log("-------------------------------------");
 }
+
+// Add buttons to play game
+const rock = document.querySelector("#rock");
+rock.addEventListener("click", () => playRound("rock"));
+
+const paper = document.querySelector("#paper");
+paper.addEventListener("click", () => playRound("paper"));
+
+const scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", () => playRound("scissors"));
